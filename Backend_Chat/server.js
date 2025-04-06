f93582313bf6b3f11dc931a30bba017ee87d4002
Gopikrishna1033 <venkatagopikrishna007@gmail.com>
2025-04-06 13:52:14 +0530

    init

--- Backend_Chat/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express")
-require("dotenv").config()
-const PORT = process.env.PORT
-const app = express()
-app.listen(PORT,()=>{console.log(`Server starts listening on ${PORT}`)})
