957e42d8ff1b5fa8bde894c7f75d38e90aed6973
Gopikrishna1033 <venkatagopikrishna007@gmail.com>
2025-04-14 19:32:43 +0530

    registration

---
+++ Backend_Chat/Backend/controllers/userControllers.js
@@ -0,0 +1,41 @@
+const asyncHandler = require("express-async-handler") // It automatically catches any errors inside your asynchronous route handlers and sends them to the error-handling middleware, simplifying error management.
+const User = require("../models/userModel")
+const generateToken = require("../token/generateToken")
+
+const registerUser = asyncHandler(async (req,res)=>{
+    console.log(req.body)
+    const {name,email,password,pic} = req.body
+
+    if (!name || !email || !password){
+        res.status(400)
+        throw new Error("Please Enter all the fields...")
+    }
+
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400)
+        throw new Error("User already Exists..")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        pic
+    })
+    if(user){
+        res.status(200).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            pic:user.pic,
+            token: generateToken(user._id)
+
+        })
+    }else{
+        res.status(400)
+        throw new Error("Failed to create the user")
+    }
+})
+
+module.exports = {registerUser}
--- Backend_Chat/Backend/server.js
+++ Backend_Chat/Backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express")
 require("dotenv").config()
 const app = express()
-const PORT = process.env.PORT
-const router = require("./routes/routes")
+const port = process.env.PORT
+const hostname = process.env.HOST_NAME;
+const mongoose = require("mongoose")
+const userRoutes = require("./routes/userRouter")
 const cors = require("cors")
 app.use(cors()) // allow all origins by default
-app.use(express.json()) // body 
-app.use("/api/chat",router)
-app.listen(PORT,()=>{
-    console.log(`Server starts running on ${PORT}...`)
+
+
+mongoose.connect(process.env.CONNECTION_STRING).then((response)=>{console.log(`Mongodb is Connected..............`)})
+.catch((err)=>{console.log(err)
+    process.exit(1)
+})
+app.use(express.json()) // body
+app.use("/api/user",userRoutes)
+app.listen(port,hostname,()=>{
+    console.log(`Server starts running on ${port}...`)
 })
